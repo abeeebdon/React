7f45c4ff783fb8f16220c5dfd14423a659b21691
Abeeb <you@example.com>
2023-07-22 18:46:31 +0100

    Making it firm

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,27 @@
-import React from 'react'
-
+import { useState} from 'react'
 const Content = () => {
+    const [name, setName] = useState('Abeeb')
+
     const handleNameChange = ( ) => {
         const names= ['Abeeb', 'Rokeeb', 'Salam','Aisha'];
         const int= Math.floor(Math.random()*3);
-        return names[int];
+        setName(names[int]);
         } 
+    const handleClick = ()=>{
+            console.log('I clicked')
+        }
+    const handleClick2= (name) => {
+            console.log(`${name} was clicked`)
+        }
+    const handleClick3= (e) => {
+            console.log(e.target.innerText)
+        }
         return (
             <main>
-            <p> Hello {handleNameChange()} </p>
+            <p> Hello {name} </p>
+            <button onClick={handleNameChange}>Change name</button>
+            <button onClick={() => handleClick2('Dave')}>Click me</button>
+            <button onClick={(e) => handleClick3(e)}>Click it</button>
             </main>)
   
 }
